e7e5390dababf7dfa9a5288732a861bf1b3fcafc
ldolberg <lautaro.dolberg@gmail.com>
2020-12-01 22:28:36 +0000

    Part2

--- day1.clj
+++ day1.clj
@@ -1,1 +1,5 @@
-(def input (slurp "input/day1"))
+(def items (sort (map #(Integer/parseInt %) (clojure.string/split-lines (slurp "input/day1")))))
+(defn first_part[arg] (filter #(some #{%} (map (fn [x] (- arg x)) items)) items))
+(println (reduce * (first_part 2020)))
+; Second Part
+(map first_part (map (fn [x] (- 2020 x)) items))
